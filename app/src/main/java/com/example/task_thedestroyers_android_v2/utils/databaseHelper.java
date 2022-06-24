9207d4c1ce9bd1966acb77b3ccfc1d89fa8692cb
iamadarsh01 <c0852822@mylambton.ca>
2022-06-23 22:34:13 -0400

    another Commit

--- app/src/main/java/com/example/task_thedestroyers_android_v2/utils/databaseHelper.java
+++ app/src/main/java/com/example/task_thedestroyers_android_v2/utils/databaseHelper.java
@@ -1,17 +1,20 @@
 package com.example.task_thedestroyers_android_v2.utils;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import com.example.task_thedestroyers_android_v2.Model.toDoModel;
+
 public class databaseHelper extends SQLiteOpenHelper {
 
     private SQLiteDatabase db;
 
     private static final String DATABASE_NAME = "TODO DATABASE";
-    private static final String TABLE_NAME = "TODO TABLE";
+    private static final String TABLE_NAME = "TODO_TABLE";
     private static final String COL_1 = "ID";
     private static final String COL_2 = "TASK";
     private static final String COL_3 = "STATUS";
@@ -26,11 +29,30 @@ public class databaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+            db.execSQL("CREATE TABLE IF NOT EXISTS " +TABLE_NAME+ "(ID PRIMARY KEY AUTOINCREMENT , TASK TEXT, STATUS INTEGER )");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+
+
+
+
+
+
+
+
+
+
+    }
+
+    public  void insertTask(toDoModel model){
+
+        db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
 
     }
 }
