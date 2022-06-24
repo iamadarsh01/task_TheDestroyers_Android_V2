35c3dd8b6496910ea5c2b96a325c88cdfb80f7bf
iamadarsh01 <c0852822@mylambton.ca>
2022-06-24 13:28:14 -0400

    another Commit

--- app/src/main/java/com/example/task_thedestroyers_android_v2/MainActivity.java
+++ app/src/main/java/com/example/task_thedestroyers_android_v2/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.task_thedestroyers_android_v2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.example.task_thedestroyers_android_v2.utils.databaseHelper;
+import com.example.task_thedestroyers_android_v2.Model.toDoModel;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private FloatingActionButton fab;
+    private databaseHelper myDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.RecyclerView);
+        fab = findViewById(R.id.fab);
+        myDB = new databaseHelper(MainActivity.this);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+
+
     }
 }
